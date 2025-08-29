b099c15006d56ad36eed3f4bad16fcad02122a09
Ali Raza <aliraza21112005@gmail.com>
2025-08-29 14:46:47 +0500

    Responding to Events

--- React/React/src/App.jsx
+++ React/React/src/App.jsx
@@ -1,20 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Hello } from './components/Hello'
 
+import './App.css'
+import { Hello } from './components/Hello'  
+import ClickFunction from './components/HelloFunction'
+import { ProductCard } from './components/ProductCard'
+import { ProductList } from './components/ProductList'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+   const products = [
+    {
+      imageSrc: "images/iphone.png",
+      title: "iPhone 15 Pro",
+      specification: [
+        "A17 Pro chip with 6-core GPU",
+        "3x or 5x Telephoto camera",
+        "Up to 29 hours video playback",
+      ],
+      price: 999,
+    },
+    {
+      imageSrc: "images/airpods.png",
+      title: "AirPods Pro 2",
+      specification: [
+        "Noise Cancellation",
+        "Dust, sweat, and water resistant",
+        "Up to 6 hours of listening",
+      ],
+      price: 249,
+    },
+    {
+      imageSrc: "images/apple-watch.png",
+      title: "Apple Watch 9",
+      specification: [
+        "45mm or 41mm case size",
+        "Always-On Retina display",
+        "Up to 18 hours normal use",
+      ],
+      price: 399,
+    },
+  ];
+
+function handlePurchase(product) {
+    alert(`You clicked on ${product.title} which cost $${product.price}`);
+  }
 
   return (
     <div className="App">
-      {/* <Greet />
-      <Welcome /> */}
-      <Hello name = 'Ali'/>
 
+      <ProductList>
+        <ProductCard width="96px"
+          height="96px"
+          background="darkolivegreen"
+          product={products[0]}
+          OnPurchase={handlePurchase}
+           />
+
+
+        <ProductCard idth="64px" height="64px" product={products[1]} OnPurchase={handlePurchase}/>
+
+        <ProductCard width="128px"
+          height="128px"
+          background="peru"
+          product={products[2]}
+          OnPurchase={handlePurchase} />
+          
+      </ProductList>
+      
+      {/* <ClickFunction /> */}
     </div>
   )
 }
