da729fab3b3d2f7f092f2243bafebd5ac5075584
Ali Raza <aliraza21112005@gmail.com>
2025-09-01 22:24:18 +0500

    TOdo List

--- React/React_States/src/App.jsx
+++ React/React_States/src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css'
-import Counter from './components/Counter'
-import LightSwitch from './components/LightSwitch'
-import LikeButton from './components/LikeButton'
+import TodoList from './components/todoList'
+
 
 function App() {
   
 
   return (
     <>
-    {/* <h2>States in React</h2> */}
-      {/* <Counter /> */}
-      {/* <LightSwitch /> */}
-      <LikeButton />
+
+      <TodoList/>
+     
     </>
   )
 }
--- React/React_States/src/components/LikeButton.jsx
+++ React/React_States/src/components/LikeButton.jsx
@@ -15,9 +15,6 @@ export default function LikeButton() {
 
                 }
             </span>
-             
-                
-    
         </h2>
     );
 }
