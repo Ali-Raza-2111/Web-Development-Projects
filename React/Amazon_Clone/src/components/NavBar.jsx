8cc930198ba4ab97ee09a41a3e2419a8dcb1d0ab
Ali Raza <aliraza21112005@gmail.com>
2025-09-08 22:02:23 +0500

    Amazon Clone Started

---
+++ React/Amazon_Clone/src/components/NavBar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import logo from "../assets/Amazon Navbar Logo.png";
+import "./NavBar.css";
+function NavBar() {
+  return (
+    <div className="Nav-Bar">
+      <div className="nav-bar-logo">
+        <img src={logo} alt="Amazon logo" />
+      </div>
+      <div className="nav-location">
+        <div className="location-icon">
+          <i class="fa-solid fa-location-dot"></i>
+          <div className="location-text">
+            <p>Deliver to </p>
+            <span>Pakistan</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="nav-search-bar">
+        <select name="search" id="">
+          <option>All</option>
+          <option>Arts and Crafts</option>
+          <option>Automotive</option>
+          <option>Books</option>
+          <option>Computers</option>
+          <option>Electronics</option>
+          <option>Health and Household</option>
+          <option>Home and Kitchen</option>
+          <option>Industrial and Scientific</option>
+          <option>Kindle Store</option>
+          <option>Mobile Phones</option>
+        </select>
+        <input type="text" className="nav-search" placeholder="Search Amazon"/>
+        <button className="nav-search-btn">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+
+      <div className="nav-language">EN</div>
+      <div className="nav-sign-in">
+        <p>Hello,Sign in </p>
+        <p>Accounts & Lists</p>
+      </div>
+      <div className="nav-return-orders">Returns & Orders</div>
+      <div className="nav-cart">
+        <i class="fa-solid fa-cart-shopping"></i>
+        <p>Cart</p>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
