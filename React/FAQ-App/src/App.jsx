497e59f6f576f3f07760a37cb3562bcb5402f72f
Ali Raza <aliraza21112005@gmail.com>
2025-09-20 01:23:27 +0500

    FAQ Center

--- React/FAQ-App/src/App.jsx
+++ React/FAQ-App/src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import FAQItem from './components/FAQItem'
+import FAQList from './components/FAQList'
 
 function App() {
+  const [darkMode,setDarkMode]=useState(false)
+  const toggleDarkMode=()=>{setDarkMode(!darkMode)}
+  useEffect(()=>{
+    if(darkMode){
+      document.documentElement.classList.add('dark')
+    }else{
+      document.documentElement.classList.remove('dark')
+    }
+  },[darkMode])
   return (
-    <div>
-      <FAQItem/>
+    <div className='min-h-screen bg-gradient-to-br from-gray-50 via-gray-100 to-gray-50 dark:from-gray-950 dark:via-gray-900 dark:to-gray-950 transition-colors duration-300'>
+      <div className='container mx-auto py-12'>
+        <header className='text-center mb-12'>
+          <h1 className='text-4xl md:text-5xl font-extrabold mb-4 bg-gradient-to-r from-blue-600 via-indigo-600 to-purple-600 bg-clip-text text-transparent inline-block'>FAQ Center</h1>
+          <p className='text-lg text-gray-600 max-w-2xl max-auto dark:text-gray-300'>Find Answers to the most common questions about Tailwind CSS and Web Development</p>
+        </header>
+      </div>
+      <FAQList toggleDarkMode={toggleDarkMode} darkMode = {darkMode}/>
+      
     </div>
   )
 }
